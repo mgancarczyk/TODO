4d32054511b08a0183b863ad3e947287532f0bf6
Michał Gancarczyk <michal.gancarczyk@microsoft.wsei.edu.pl>
2019-03-20 18:26:49 +0100

    MyCommit

---
+++ script/script.js
@@ -0,0 +1,14 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCOv7g1c5Li5hd2cr36oNAZKjineEJ98JU",
+    authDomain: "todo-mg.firebaseapp.com",
+    databaseURL: "https://todo-mg.firebaseio.com",
+    projectId: "todo-mg",
+    storageBucket: "todo-mg.appspot.com",
+    messagingSenderId: "1073053472754"
+  };
+  firebase.initializeApp(config);
+
+  //var response = prompt("What is ?");
+
+   // alert("Hello, " + response);
